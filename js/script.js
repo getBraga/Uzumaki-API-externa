bec940c023ebd8523bad1b091908f9776145873e
Ysmael Braga <ysmaelbraga01@gmail.com>
2020-04-07 00:01:33 -0300

    Foi feito no javascript o algoritmo para a animação do h1

--- js/script.js
+++ js/script.js
@@ -5,20 +5,28 @@ async function buscaFetch() {
   const aUrl = document.querySelectorAll('a')
   const h2 = document.querySelectorAll('h2');
   const  sinopse = document.querySelectorAll('.sinopse');
-  const episodios = document.querySelectorAll('.episodios')
-
-  console.log(url);
+  const episodios = document.querySelectorAll('.episodios');
   for(let i = 0; i< url.results.length; i++ ) {
     imgs[i].src = url.results[i].image_url;
     h2[i].innerText =url.results[i].title; 
     sinopse[i].innerText =url.results[i].synopsis; 
     episodios[i].innerText =`Episodios:  ${url.results[i].episodes}`;
-    aUrl[i].href = url.results[i].url ;
-  
-   
-    
+    aUrl[i].href = url.results[i].url ; 
   }
 
 }
 
+function efeitoEscrever() {
+  const h1 = document.querySelector('h1');
+  const h1Escreve = h1.innerHTML.split('');
+  h1.innerHTML= '';
+  h1Escreve.forEach((p, i) => {
+    setTimeout(() => {
+      h1.innerHTML += p;
+    },80*i);
+  }) 
+ 
+  
+}
+efeitoEscrever();
 buscaFetch();
